f0973468bb70a9c2bcae93d6b30e54bad7859abc
Anand mohan awasthi <anand.awasthi.csharp@gmail.com>
2019-11-27 12:07:49 +0530

    Delete service.py

--- Coward Counting/service.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-url = 'http://localhost:5000/api/'
-
-headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-image_path = 'D://Personal//Hackathon//Coward Counting//test//IMG_3.jpg'
-r = requests.get(url, data = image_path, headers=headers)
-print(r, r.text)
